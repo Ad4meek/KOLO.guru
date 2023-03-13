8dce58c5ae8f71eaf30536726a707ecbb325b3da
adam <adam.erben@seznam.cz>
2023-03-13 18:19:27 +0100

    Init

---
+++ res/js/main.js
@@ -0,0 +1,7 @@
+const navMenu = document.querySelector('.nav__menu');
+const sidePanel = document.querySelector('.side_panel');
+
+navMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('menuActive');
+    sidePanel.classList.toggle('menuActive');
+})
