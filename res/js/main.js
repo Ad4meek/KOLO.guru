11eab93f8b447d0f8e0ccddb3dd2f39165815382
adam <adam.erben@seznam.cz>
2023-03-16 09:43:17 +0100

    Footer

--- res/js/main.js
+++ res/js/main.js
@@ -1,7 +1,7 @@
-const navMenu = document.querySelector('.nav__menu');
-const sidePanel = document.querySelector('.side_panel');
+const navMenu = document.querySelector(".nav__menu");
+const sidePanel = document.querySelector(".side_panel");
 
-navMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('menuActive');
-    sidePanel.classList.toggle('menuActive');
-})
+navMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("menuActive");
+  sidePanel.classList.toggle("menuActive");
+});
